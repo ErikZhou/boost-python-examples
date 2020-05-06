b6a4352d8f36b93e16677891dd806d67d5e09e67
EricZhou <qqzhoucn@gmail.com>
2020-05-06 10:49:52 +0800

    Update classes.py

--- 02-ExposingClasses/classes.py
+++ 02-ExposingClasses/classes.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
 import classes
+import time
 
 t = classes.World()
+msg = t.many(['Good Morning', 'Buon giorno', 'Kali mera'])
+print(msg)
+print(t.greet())
+print("Start : %s" % time.ctime())
+time.sleep(5)
+print("End : %s" % time.ctime())
 t.set("bom dia!")
 print (t.greet())
-
-t.many(['Good Morning', 'Buon giorno', 'Kali mera'])
-print (t.greet())
+print('---------------Done---------------')
 
 
