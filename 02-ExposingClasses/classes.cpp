9602e1f6cbf4636722f7d44f2e6605e9d88c1f58
EricZhou <qqzhoucn@gmail.com>
2020-05-06 10:48:21 +0800

    Update classes.cpp

--- 02-ExposingClasses/classes.cpp
+++ 02-ExposingClasses/classes.cpp
@@ -5,22 +5,48 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <iostream>
+#include <ctime>
+using namespace std;
 
-struct World
+class World
 {
-    void set(std::string msg) { mMsg = msg; }
-    void many(boost::python::list msgs) {
-        long l = len(msgs);
-        std::stringstream ss;
-        for (long i = 0; i<l; ++i) {
-            if (i>0) ss << ", ";
-            std::string s = boost::python::extract<std::string>(msgs[i]);
-            ss << s;
-        }
-        mMsg = ss.str();
+public:
+	void set(std::string msg) 
+    {
+        current_time();
+        printf("C++ set begin\n");
+        mMsg = msg; 
+        printf("C++ set end\n");
     }
-    std::string greet() { return mMsg; }
-    std::string mMsg;
+    void current_time()
+    {
+        time_t curr_time;
+        curr_time = time(NULL);
+
+        tm *tm_local = localtime(&curr_time);
+        cout << "\nCurrent local time : " << tm_local->tm_hour << ":" << tm_local->tm_min << ":" << tm_local->tm_sec << "\n";
+    }
+	std::string many(boost::python::list msgs) {
+        current_time();
+		long l = len(msgs);
+        printf("C++ begin %d\n", l);
+		std::stringstream ss;
+		for (long i = 0; i < l; ++i) {
+			if (i > 0) ss << ", ";
+			std::string s = boost::python::extract<std::string>(msgs[i]);
+			ss << s;
+		}
+		mMsg = ss.str();
+        printf("C++ end mMsg=%s\n", mMsg.c_str());
+
+        return mMsg;
+	}
+	std::string greet() 
+    {
+        return mMsg; 
+    }
+	std::string mMsg;
 };
 
 using namespace boost::python;
